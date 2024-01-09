8b5c932ce02696651484cd88b8a4a5945e17383a
Landung 'Don' Setiawan <landungs@uw.edu>
2024-01-09 15:33:36 -0800

    feat: Added state dictionary for issues created

--- load_issues.py
+++ load_issues.py
@@ -1,10 +1,13 @@
+from collections import OrderedDict
 from pathlib import Path
 from typing import Any, Dict, List
 import os
 import textwrap
+import time
 
 import typer
 import yaml
+import msgpack
 
 from github import Github
 from github import Auth
@@ -98,32 +101,59 @@ def parse_issue_template(codeuw_repo: Repository) -> Dict[str, Any]:
 def generate_code_uw_issues(
     issues_with_label: List[Issue],
     template_dict: Dict[str, Any],
-    repo: Dict[str, Any],
-    codeuw_issue_titles: List[str],
+    project_name: str,
+    codeuw_state: OrderedDict,
     gh_repo: Repository,
     codeuw_repo: Repository,
     dry_run: bool = False,
-) -> None:
+) -> OrderedDict:
+    """Generate codeuw issues from
+    the issues with codeuw label
+
+    Parameters
+    ----------
+    issues_with_label : List[Issue]
+        The list of ``Issue`` objects with codeuw label
+    template_dict : Dict[str, Any]
+        The template dictionary to create issue
+    project_name : str
+        The repository custom project name from config
+    codeuw_state : OrderedDict
+        The codeuw state dictionary
+    gh_repo : Repository
+        The Github repository object to get issues from
+    codeuw_repo : Repository
+        The Github repository object of the codeuw repo
+    dry_run : bool, optional
+        Flag to signify a dry run, which doesn't create issues
+        within the codeuw repo, by default False
+        
+    Returns
+    -------
+    OrderedDict
+        The updated codeuw state dictionary
+    """
     # Loop over issues and create one by one
     for issue in issues_with_label:
         issue_template = template_dict.copy()
         issue_title = issue_template["title"](
-            project_name=repo["name"], title_text=issue.title
+            project_name=project_name, title_text=issue.title
         )
+        issue_creator = issue.user.login
 
         # Skip the rest if issue already exists
-        if issue_title in codeuw_issue_titles:
+        if issue.number in codeuw_state["issues"][gh_repo.full_name]:
             logger.info(f"Issue already exists in codeuw repo: {issue_title}")
             continue
 
         issue_template["title"] = issue_title
         issue_template["body"] = issue_template["body"](
-            contact="*Please provide your contact information here.*",
+            contact=f"@{issue_creator}",
             description=issue.body if issue.body else "*No description provided.*",
             repo=gh_repo.html_url,
             issue=issue.html_url,
-            level="*Please provide the level of the task here.*",
-            language="*Please provide the programming language of the task here.*",
+            level=f"*@{issue_creator}: Please provide the level of the task here.*",
+            language=f"*@{issue_creator}: Please provide the programming language of the task here.*",
             dependencies="*No response*",
         )
 
@@ -132,10 +162,45 @@ def generate_code_uw_issues(
         if not dry_run:
             created_issue = codeuw_repo.create_issue(**issue_template)
             logger.info(f"Issue successfully created: {created_issue.html_url}")
+            codeuw_state["issues"][gh_repo.full_name][issue.number] = created_issue.number
         else:
             logger.info("Dry run, not creating issue. Here is the issue body:\n")
             logger.info("\n" + issue_template["body"])
+            codeuw_state["issues"][gh_repo.full_name][issue.number] = -1
+        codeuw_state["last_modified"] = int(time.time())
+    return codeuw_state
 
+def get_state(state_file: str = '.codeuw-state.mpk') -> OrderedDict:
+    """
+    Get the state dictionary from the state file
+    """
+    state_file = Path(state_file)
+    if state_file.exists():
+        # Read the state file when it already exists
+        return read_state(state_file=state_file)
+    else:
+        # If state file doesn't exist, create a new one
+        codeuw_state = OrderedDict({
+            "version": "1.0",
+            "created_time": int(time.time()),
+            "last_modified": int(time.time()),
+            "issues": {}
+        })
+        return codeuw_state
+
+def write_state(state_dict: OrderedDict, state_file: "str | Path" = '.codeuw-state.mpk') -> None:
+    """
+    Write state file to disk as messagepack file format
+    """
+    state_file = Path(state_file)
+    state_file.write_bytes(msgpack.packb(state_dict))
+    
+def read_state(state_file: "str | Path" = '.codeuw-state.mpk') -> OrderedDict:
+    """
+    Read state file from disk as messagepack file format
+    """
+    state_file = Path(state_file)
+    return msgpack.unpackb(state_file.read_bytes(), strict_map_key=False)
 
 def main(config_file: str = ".codeuw-config.yml", dry_run: bool = False):
     """
@@ -146,15 +211,19 @@ def main(config_file: str = ".codeuw-config.yml", dry_run: bool = False):
 
     # Get codeuw repo
     codeuw_repo = gh.get_repo("/".join([config["owner"], config["repo"]]))
-
-    # Get all issue titles from codeuw repo
-    codeuw_issue_titles = [issue.title for issue in codeuw_repo.get_issues(state="all")]
+    
+    # Get the state dictionary
+    codeuw_state = get_state()
 
     # Get the template dictionary
     template_dict = parse_issue_template(codeuw_repo)
 
     for repo in config["repos"]:
         repo_path = "/".join([repo["org"], repo["repo"]])
+        # If repo not in state, add it
+        if repo_path not in codeuw_state["issues"]:
+            codeuw_state["issues"][repo_path] = {}
+
         gh_repo = gh.get_repo(repo_path)
         # Get issues with codeuw label only
         issues_with_label = [
@@ -162,15 +231,20 @@ def main(config_file: str = ".codeuw-config.yml", dry_run: bool = False):
             for issue in gh_repo.get_issues()
             if "codeuw" in [label.name for label in issue.labels]
         ]
-        generate_code_uw_issues(
+        codeuw_state = generate_code_uw_issues(
             issues_with_label,
             template_dict,
-            repo,
-            codeuw_issue_titles,
+            repo['name'],
+            codeuw_state,
             gh_repo,
             codeuw_repo,
             dry_run,
         )
+    
+    logger.info(f"State content:\n {codeuw_state}")
+    if not dry_run:
+        logger.info("Writing state file to disk")
+        write_state(codeuw_state)
 
 
 if __name__ == "__main__":
